8851488d685ce07d1babd6ea8dd42116afb2fc96
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2024-02-17 18:45:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
+# @title Appwrite Code
 
 import re
 from appwrite.client import Client
 from appwrite.services.databases import Databases
-from ytmusicapi import YTMusic
-
 from time import sleep
 from pytube import YouTube
 import os
 
-# Function to download video using pytube
-def download_video(url, output_path):
-    yt = YouTube(url)
-    video = yt.streams.get_highest_resolution()
-    video.download(output_path)
-    return os.path.join(output_path, video.title + ".mp4")
-
-# Function to convert video to m4a using ffmpeg
-def convert_to_m4a(input_file, output_file):
-    command = f'ffmpeg -i "{input_file}" -vn -acodec copy "{output_file}"'
-    os.system(command)
-
 def download_directly(video_url,custom_title):
   output_path = os.getcwd()  # Use the current working directory as the output path
-  video_file = download_video(video_url, output_path)
   output_audio_file = os.path.join(output_path, f"{custom_title}")
-  convert_to_m4a(video_file, output_audio_file)
+
+  yt = YouTube(url)
+  audio_stream_low_mb = yt.streams.filter(only_audio=True).first()
+  audio_stream_high_mb = yt.streams.filter(only_audio=True).last()
+  audio_stream_high_mb.download(output_path=os.getcwd(), filename={custom_title})
+
   return output_audio_file
+
 
 def get_video_id(link):
   url = link
@@ -59,7 +51,7 @@ def create_artist(document_id, name, image_url):
 
 
 
-def create_album(document_id, name, year, artworkurl, artist_id,album_type):
+def create_album(document_id, name, year, artworkurl, artist_id, album_type, artistimageurl,artistname):
   client = Client()
 
   (client
@@ -75,14 +67,17 @@ def create_album(document_id, name, year, artworkurl, artist_id,album_type):
       "year": year,
       "artworkurl": artworkurl,
       "artist": artist_id,
-      "type":album_type
+      "type": album_type,
+      "artistid": artist_id,
+      "artistimageurl": artistimageurl,
+      "artistname" : artistname
   }
 
   result = databases.create_document('gracedb', 'albums', document_id, data)
 
 
 
-def create_song(document_id, name, language, length, audiourl, album_id,album_image_url, artist_name):
+def create_song(document_id, name, language, length, audiourl, album_id,album_image_url, artist_name, artistimageurl, albumname, albumyear,albumtype):
   client = Client()
 
   (client
@@ -98,12 +93,15 @@ def create_song(document_id, name, language, length, audiourl, album_id,album_im
       "language" : language,
       "audiourl" : audiourl,
       "album" : album_id,
-      "albumartwork" : album_image_url,
-      "albumartistname": artist_name
+      "albumartwork":album_image_url,
+      "albumartistname" : artist_name,
+      "albumid": album_id,
+      "artistimageurl": artistimageurl,
+      "albumname": albumname,
+      "albumyear":albumyear ,
+      "albumtype": albumtype
   }
   result = databases.create_document('gracedb', 'songs', document_id, data)
-
-
 
 
 
@@ -118,19 +116,20 @@ bot_token = '6361645598:AAF8Lioo2hS7tsGPXT0bTn5uT9pGF0gMAB8'
 
 url = f"https://api.telegram.org/bot{bot_token}/sendAudio"
 headers = {"accept":"application/json"}
-data = {"chat_id":"1876292868"}
 
 
-def upload_audio_and_get_link(audio_path, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name):
+def upload_audio_and_get_link(audio_path, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artistimageurl,albumname,albumyear,albumtype):
   # Replace 'YOUR_CHAT_ID' with the chat ID where you want to send the audio
   chat_id = '1876292868'
 
   # Replace 'audio_path' with the path to your music/audio file
   audio_path = audio_path
 
+  data = {"chat_id":"1876292868", 'file_unique_id': track_document_id}
 
-  # Upload the audio file and get the file ID
-  with open(audio_path, 'rb') as audio_file:
+  try:
+   # Upload the audio file and get the file ID
+    with open(audio_path, 'rb') as audio_file:
       files = {"audio": audio_file}
       resp = requests.post(url, headers=headers, params=data, files=files)
       resp = resp.json()
@@ -138,25 +137,19 @@ def upload_audio_and_get_link(audio_path, track_document_id, track_name, track_d
       file_id = resp['result']['audio']['file_id']
       audio_file_id = file_id
 
-  url2 = f"https://api.telegram.org/bot{bot_token}/getFile?file_id={audio_file_id}"
-  file_info = requests.get(url2).json()
-  file_path = file_info['result']['file_path']
+    url2 = f"https://api.telegram.org/bot{bot_token}/getFile?file_id={audio_file_id}"
+    file_info = requests.get(url2).json()
+    file_path = file_info['result']['file_path']
 
-  # Step 2: Construct the direct link
-  file_url = f"https://api.telegram.org/file/bot{bot_token}/{file_path}"
-  language = "Telugu"
-  track_duration_ms = str(track_duration_ms)
-  create_song(track_document_id,track_name,language,track_duration_ms,file_url,album_document_id,album_image_url, artist_name)
+    # Step 2: Construct the direct link
+    file_url = f"https://api.telegram.org/bot{bot_token}/{file_path}"
+    language = "Telugu"
+    track_duration_ms = str(track_duration_ms)
+    create_song(track_document_id,track_name,language,track_duration_ms,file_url,album_document_id,album_image_url, artist_name,artistimageurl,albumname,albumyear,albumtype)
+    sleep(9)
+  except:
+    raise ValueError('Telegram Error')
   sleep(9)
-
-# Create and run the event loop
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(upload_audio_and_get_link())
-
-
-
-
-
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -185,7 +178,7 @@ def get_odesli_info(spotify_track_link):
     return odesli_data
 
 def search_youtube_video(track_name, artist_name):
-    query = f"{track_name} {artist_name}"
+    query = f"{track_name} {artist_name} official video"
     video_search = VideosSearch(query, limit = 1)
 
     results = video_search.result()
@@ -193,6 +186,8 @@ def search_youtube_video(track_name, artist_name):
         return results['result'][0]['link']
     else:
         return 'N/A'
+
+from ytmusicapi import YTMusic
 
 def search_youtube_music(track_name, artist_name):
     ytmusic = YTMusic()
@@ -220,6 +215,7 @@ def get_artist_albums_and_songs(client_id, client_secret, artist_id):
     artist_images = artist['images'] if 'images' in artist else []
     artist_image_url = artist_images[0]['url'] if artist_images else 'N/A'
 
+
     artist_name_without_commas = artist_name.replace(',', '')
     rand = random.randint(1,99)
     artist_document_id = artist_name_without_commas.replace(" ", "")+str(rand)
@@ -239,10 +235,10 @@ def get_artist_albums_and_songs(client_id, client_secret, artist_id):
         album_images = album['images'] if 'images' in album else []
         album_image_url = album_images[0]['url'] if album_images else 'N/A'
 
-        text_without_commas = album_name.replace(',', '')
+        album_name_without_commas = album_name.replace(',', '')
         rand = random.randint(1,99)
-        album_document_id = text_without_commas.replace(" ", "")+str(rand)
-        create_album(album_document_id,album_name,album_year,album_image_url,artist_document_id,album_type)
+        album_document_id = album_name_without_commas.replace(" ", "")+str(rand)
+        create_album(album_document_id,album_name,album_year,album_image_url,artist_document_id,album_type,artist_image_url,artist_name)
 
         print(f"\nAlbum: {album_name}, Year: {album_year}")
         print(f"Album Image URL: {album_image_url}")
@@ -274,58 +270,76 @@ def get_artist_albums_and_songs(client_id, client_secret, artist_id):
             print(f"  YouTube Link: {youtube_link}")
 
             # Search for the video on YouTube and get the link
-            youtube_video_link = search_youtube_video(track_name, artist_name)
+            youtube_video_link = search_youtube_music(track_name, artist_name)
             print(f"  YouTube Video Link: {youtube_video_link}")
 
             try:
               filepath = track_name+'-'+ get_video_id(youtube_music_link)+'.m4a'
-              upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id,album_image_url, artist_name )
+              upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id,album_image_url, artist_name, artist_image_url,album_name, album_year, album_type)
+            except ValueError:
+              continue
             except:
               try:
                 filepath = track_name+'-'+ get_video_id(youtube_link)+'.m4a'
-                upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name)
+                upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artist_image_url,album_name, album_year, album_type)
+              except ValueError:
+                continue
               except:
                 try:
                   filepath = track_name+'-'+ get_video_id(youtube_video_link)+'.m4a'
-                  upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name)
+                  upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artist_image_url,album_name, album_year, album_type)
+                except ValueError:
+                  continue
                 except:
                   try:
                     os.system(f'youtube-dl {youtube_music_link} --extract-audio --audio-format m4a --audio-quality 128K')
                     filepath = track_name+'-'+ get_video_id(youtube_music_link)+'.m4a'
-                    upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name)
+                    upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artist_image_url,album_name, album_year, album_type)
+                  except ValueError:
+                    continue
                   except:
                     try:
                       filepath = track_name+'-'+ get_video_id(youtube_link)+'.m4a'
                       os.system(f'youtube-dl {youtube_link} --extract-audio --audio-format m4a --audio-quality 128K')
-                      upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name)
+                      upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artist_image_url,album_name, album_year, album_type)
+                    except ValueError:
+                      continue
                     except:
                       try:
                         filepath = track_name+'-'+ get_video_id(youtube_music_link)+'.m4a'
                         download_directly(youtube_music_link,filepath)
-                        upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name)
+                        upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artist_image_url,album_name, album_year, album_type)
+                      except ValueError:
+                        continue
                       except:
                         try:
                           filepath = track_name+'-'+ get_video_id(youtube_video_link)+'.m4a'
                           os.system(f'youtube-dl {youtube_video_link} --extract-audio --audio-format m4a --audio-quality 128K')
-                          upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id,album_image_url, artist_name )
+                          upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id,album_image_url, artist_name,artist_image_url ,album_name, album_year, album_type)
+                        except ValueError:
+                          continue
                         except:
                           try:
                             filepath = track_name+'-'+ get_video_id(youtube_link)+'.m4a'
                             download_directly(youtube_link,filepath)
-                            upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name)
+                            upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artist_image_url,album_name, album_year, album_type)
+                          except ValueError:
+                            continue
                           except:
                             try:
                               filepath = track_name+'-'+ get_video_id(youtube_video_link)+'.m4a'
                               download_directly(youtube_video_link,filepath)
-                              upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id,album_image_url, artist_name )
+                              upload_audio_and_get_link(filepath, track_document_id, track_name, track_duration_ms, album_document_id ,album_image_url, artist_name,artist_image_url,album_name, album_year, album_type)
+                            except ValueError:
+                              continue
                             except:
                               pass
 
-
-
 if __name__ == "__main__":
-    #get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "1wAnjuaT1lZ9ULjRJIq3rX")
-    #get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "1XaWLxLjDeXKZx4LmHsQQf")
+    get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, ARTIST_ID)
+    get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "1wAnjuaT1lZ9ULjRJIq3rX")
+    '''
+    get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "1XaWLxLjDeXKZx4LmHsQQf")
     get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "1w8S83yGWdb6sN3s6mjg28")
     get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "02m3PiiWBO9hWspWxZqB7k")
     get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "7zRvtToaWinJSv96cbx3YU")
@@ -337,4 +351,4 @@ if __name__ == "__main__":
     get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "42LJGLdvmjG2teUzhLkV48")
     get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "2ZI6RrNaij9iVXKcV6kgsv")
     get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "3nr5yEkaBmm6tVAgiuCr7W")
-    get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "5imstv16Uz57btzlivDf9d")
+    get_artist_albums_and_songs(CLIENT_ID, CLIENT_SECRET, "5imstv16Uz57btzlivDf9d")'''
